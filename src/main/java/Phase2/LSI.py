80f766bac8edc300573886fc0f26a37b68b18d89
Diogenis <dio.tsolakou@hotmail.com>
2021-05-22 20:22:05 +0300

    Still takes too much memory

--- src/main/java/Phase2/LSI.py
+++ src/main/java/Phase2/LSI.py
@@ -1,9 +1,28 @@
 import numpy as np
 import pandas as pd
+#import textmining as tm
+
+
+def read_file():
+    docxterm = np.loadtxt("../../../../docXterm.txt")
+    termxdoc = np.array(docxterm.T, dtype=int)
+    print("txd shape :", termxdoc.shape)
+    return termxdoc
+
+
+def svd_analysis(array):
+    u, s, v = np.linalg.svd(array)
+    print("u shape :", u.shape)
+    print("s shape :", s.shape)
+    print("v shape :", v.shape)
+    return u, s, v
 
 
 def main():
-    return
+    txd = read_file()
+    u, v, t = svd_analysis(txd)
+
+    #txd.write_csv('matrix.csv', cutoff=1)
 
 
 if __name__ == "__main__":

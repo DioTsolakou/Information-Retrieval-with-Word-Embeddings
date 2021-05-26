77caf2b08915d0f4bd71d5b6d9d2d76059c61160
Diogenis <dio.tsolakou@hotmail.com>
2021-05-26 17:12:20 +0300

    Supposedly complete

--- src/main/java/Phase2/LSI.py
+++ src/main/java/Phase2/LSI.py
@@ -1,7 +1,34 @@
+import functools
+from operator import itemgetter
 import numpy as np
-import sys
 #import pandas as pd
 #import textmining as tm
+
+
+class QueryData:
+    def __init__(self, id):
+        self.id = id
+
+
+class DocData:
+    def __init__(self, id):
+        self.id = id
+
+
+class ScoreDocs:
+    def __init__(self, qid, docid, score):
+        self.qid = qid
+        self.docid = docid
+        self.score = score
+
+    def get_qid(self):
+        return self.qid
+
+    def get_docid(self):
+        return self.docid
+
+    def get_score(self):
+        return self.score
 
 
 def read_files():
@@ -44,6 +71,41 @@ def calculate_cos_similarity(uk, sk, vk, txq):
     return similarity
 
 
+def cmpsd(sd1, sd2):
+    if sd1.get_qid() > sd2.get_qid():
+        return 1
+    elif sd1.get_qid() < sd2.get_qid():
+        return -1
+    else:
+        if sd1.get_score() > sd2.get_score():
+            return -1
+        elif sd1.get_score() < sd2.get_score():
+            return 1
+        else:
+            return 0
+
+
+def create_file_for_trec_eval(array, topK):
+    file = open("our_results_{0}.txt".format(topK), "x")
+    scoreDocsList = []
+    for i in range(array.shape[0]):
+        qList = []
+        for j in range(array.shape[1]):
+            qList.append(ScoreDocs(i + 1, j + 1, array[i][j]))
+        sortedQ = sorted(qList, key=itemgetter('score'))
+        topKQ = sortedQ[:topK]
+        scoreDocsList.extend(topKQ)
+
+    for t in scoreDocsList:
+        if t.get_qid() < 10:
+            t.qid = "0" + t.get_qid()
+        while len(t.get_docid()) < 4:
+            t.docid = "0" + t.get_docid()
+        file.write("{0}\t0\t{1}\t0\t{2}\tstandard_run_id\n".format(t.get_qid(), t.get_docid(), t.get_score()))
+
+    file.close()
+
+
 def main():
     txd, txq = read_files()
     u, s, v = svd_analysis(txd)
@@ -54,11 +116,20 @@ def main():
     #print("txd_test contents : ", txd_test)
     #print((txd == txd_test).all())
 
-    rank = sys.argv[1]
+    rank = input("Give rank approximation : ")
+    rank = int(rank)
+    topK = input("Give topK docs")
+    topK = int(topK)
+
+    np.savetxt('u_matrix.txt', u, delimiter=' ')
+    np.savetxt('s_matrix.txt', s, delimiter=' ')
+    np.savetxt('v_matrix.txt', v, delimiter=' ')
+
     ak, uk, sk, vk = create_ranked_matrix(u, s, v, rank)
     np.savetext('ak_matrix.txt', ak, delimiter=' ')
     similarity_matrix = calculate_cos_similarity(uk, sk, vk, txq)
     np.savetext('similarity_matrix.txt', similarity_matrix, delimiter=' ')
+    create_file_for_trec_eval(similarity_matrix, topK)
 
 
 if __name__ == "__main__":

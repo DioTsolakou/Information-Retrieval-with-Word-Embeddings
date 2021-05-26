340db14a988012ee04eaa6b492584930acc713a8
Diogenis Tsolakou <32932164+diotsolakou@users.noreply.github.com>
2021-05-26 19:53:45 +0300

    Finished!

--- src/main/java/Phase2/LSI.py
+++ src/main/java/Phase2/LSI.py
@@ -1,8 +1,5 @@
-import functools
-from operator import itemgetter
+import os
 import numpy as np
-#import pandas as pd
-#import textmining as tm
 
 
 class QueryData:
@@ -43,6 +40,11 @@ def read_files():
     return termxdoc, termxqueries
 
 
+def read_svd_matrix(txtfile):
+    matrix = np.loadtxt("{0}".format(txtfile))
+    return np.array(matrix)
+
+
 def svd_analysis(array):
     u, s_tmp, v = np.linalg.svd(array)
     s = np.zeros(array.shape)
@@ -59,15 +61,17 @@ def create_ranked_matrix(u, s, v, rank):
     vk = v[:rank, :]
     ak = np.dot(np.dot(uk, sk), vk)
     ak = np.around(ak)
+    print("Finished creation of ranked matrices")
     return ak, uk, sk, vk
 
 
 def calculate_cos_similarity(uk, sk, vk, txq):
-    qTk = np.dot(np.dot(txq.T, uk), sk) # qTk = (64, k)
-    enumerator = np.dot(qTk, vk) # enum = (64, 3204) = qxd
+    qTk = np.dot(np.dot(txq.T, uk), sk)  # qTk = (64, k)
+    enumerator = np.dot(qTk, vk)  # enum = (64, 3204) = qxd
     denominator = np.linalg.norm(qTk) * np.linalg.norm(vk)
-    similarity = enumerator/denominator
+    similarity = enumerator / denominator
     similarity = np.array(similarity)
+    print("Finished calculation of cosine similarity")
     return similarity
 
 
@@ -85,51 +89,55 @@ def cmpsd(sd1, sd2):
             return 0
 
 
-def create_file_for_trec_eval(array, topK):
-    file = open("our_results_{0}.txt".format(topK), "x")
+def create_file_for_trec_eval(array, topK, rank):
+    array = np.around(array, 5)
+
+    if os.path.isfile("our_results_top{0}_r{1}.txt".format(topK, rank)):
+        os.remove("our_results_top{0}_r{1}.txt".format(topK, rank))
+
+    file = open("our_results_top{0}_r{1}.txt".format(topK, rank), "x")
     scoreDocsList = []
     for i in range(array.shape[0]):
         qList = []
         for j in range(array.shape[1]):
             qList.append(ScoreDocs(i + 1, j + 1, array[i][j]))
-        sortedQ = sorted(qList, key=itemgetter('score'))
+        sortedQ = sorted(qList, key=lambda x: (x.get_score()), reverse=True)
         topKQ = sortedQ[:topK]
         scoreDocsList.extend(topKQ)
 
     for t in scoreDocsList:
         if t.get_qid() < 10:
-            t.qid = "0" + t.get_qid()
-        while len(t.get_docid()) < 4:
-            t.docid = "0" + t.get_docid()
+            t.qid = "0" + str(t.get_qid())
+        while len(str(t.get_docid())) < 4:
+            t.docid = "0" + str(t.get_docid())
         file.write("{0}\t0\t{1}\t0\t{2}\tstandard_run_id\n".format(t.get_qid(), t.get_docid(), t.get_score()))
 
     file.close()
+    print("Finished creating readable file for trec eval")
 
 
 def main():
     txd, txq = read_files()
     u, s, v = svd_analysis(txd)
-    txd_test = np.array(np.dot(np.dot(u, s), v))
-    txd_test = np.around(txd_test)
-    print("txd_test shape : ", txd_test.shape)
-    #print("txd contents : ", txd)
-    #print("txd_test contents : ", txd_test)
-    #print((txd == txd_test).all())
 
-    rank = input("Give rank approximation : ")
+    choice = int(input("Do you want to save the u, s and v matrices from svd?"
+                       "\n Press 1 for yes, 0 for no\nCaution : Large file size"))
+
+    if choice == 1:
+        np.savetxt('u_matrix.txt', u, delimiter=' ')
+        np.savetxt('s_matrix.txt', s, delimiter=' ')
+        np.savetxt('v_matrix.txt', v, delimiter=' ')
+
+    rank = input("Give rank approximation : ")  # 50, 150, 300
     rank = int(rank)
-    topK = input("Give topK docs")
+    topK = input("Give topK docs : ")  # 20, 30, 50
     topK = int(topK)
 
-    np.savetxt('u_matrix.txt', u, delimiter=' ')
-    np.savetxt('s_matrix.txt', s, delimiter=' ')
-    np.savetxt('v_matrix.txt', v, delimiter=' ')
-
     ak, uk, sk, vk = create_ranked_matrix(u, s, v, rank)
-    np.savetext('ak_matrix.txt', ak, delimiter=' ')
+    np.savetxt('ak_matrix.txt', ak, delimiter=' ')
     similarity_matrix = calculate_cos_similarity(uk, sk, vk, txq)
-    np.savetext('similarity_matrix.txt', similarity_matrix, delimiter=' ')
-    create_file_for_trec_eval(similarity_matrix, topK)
+    np.savetxt('similarity_matrix.txt', similarity_matrix, delimiter=' ')
+    create_file_for_trec_eval(similarity_matrix, topK, rank)
 
 
 if __name__ == "__main__":
